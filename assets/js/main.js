cb22a93f21706329994f0637489dfcb964196a66
Beyrek <nagilibeyrek@gmail.com>
2025-12-16 13:59:50 +0400

    luplupluplup

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * assets/js/main.js
+ * Veb sayt üçün əsas JavaScript funksiyaları
+ */
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Portfolio Veb Saytı yükləndi. Bütün elementlər hazırdır!");
+    // Əlavə funksionallıq tələb olunarsa, bura əlavə edilə bilər.
+});
